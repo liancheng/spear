44ffbdca0311069286c47625ac4d1010b7979451
Cheng Lian <lian@databricks.com>
2015-11-22 18:43:55 +0800

    Adds logical operator If

--- src/main/scala/scraper/expressions/logicalOperators.scala
+++ src/main/scala/scraper/expressions/logicalOperators.scala
@@ -4,6 +4,7 @@ import scala.util.Try
 
 import scraper.Row
 import scraper.expressions.Cast.promoteDataTypes
+import scraper.types.DataType
 
 trait BinaryLogicalPredicate extends Predicate with BinaryExpression {
   override lazy val strictlyTyped: Try[Expression] = {
@@ -39,4 +40,26 @@ case class Not(child: Predicate) extends UnaryPredicate {
   override lazy val strictlyTyped: Try[Expression] = for {
     Predicate(e) <- child.strictlyTyped
   } yield copy(child = e)
+}
+
+case class If(condition: Predicate, trueValue: Expression, falseValue: Expression)
+  extends Expression {
+
+  override def dataType: DataType = whenStrictlyTyped(trueValue.dataType)
+
+  override def strictlyTyped: Try[Expression] = for {
+    Predicate(c) <- condition.strictlyTyped
+    t <- trueValue.strictlyTyped
+    f <- falseValue.strictlyTyped
+    (promotedT, promotedF) <- promoteDataTypes(t, f)
+  } yield If(c, promotedT, promotedF)
+
+  override def evaluate(input: Row): Any =
+    if (condition.evaluate(input).asInstanceOf[Boolean]) {
+      trueValue.evaluate(input)
+    } else {
+      falseValue.evaluate(input)
+    }
+
+  override def children: Seq[Expression] = Seq(condition, trueValue, falseValue)
 }
