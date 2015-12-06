436a95265b3744239e3a9e48c710871486bd1346
Cheng Lian <lian@databricks.com>
2015-12-06 12:57:01 +0800

    Rewrites TreeNodeSuite using random testing style

--- src/test/scala/scraper/trees/TreeNodeSuite.scala
+++ src/test/scala/scraper/trees/TreeNodeSuite.scala
@@ -1,105 +1,134 @@
 package scraper.trees
 
-import scala.collection.JavaConversions._
+import scala.collection.Iterator.iterate
+import scala.language.implicitConversions
 
-import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Prop.{BooleanOperators, all, forAll}
+import org.scalacheck.util.Pretty
+import org.scalacheck.{Arbitrary, Gen, Test}
 import org.scalatest.prop.Checkers
 
 import scraper.LoggingFunSuite
+import scraper.trees.TreeNodeSuite.Node
 import scraper.types.TestUtils
 
-case class Node(value: Int, children: Seq[Node]) extends TreeNode[Node] {
-  override def nodeCaption: String = s"Node($value)"
-}
-
 class TreeNodeSuite extends LoggingFunSuite with TestUtils with Checkers {
-  // A generator that generates trees with a max depth of 10 and a max fan-out of 2.
-  private val genNode = {
-    def gen(maxDepth: Int): Gen[Node] = for {
-      fanOut <- if (maxDepth > 0) Gen.choose(0, 3) else Gen.const(0)
-      value <- Gen.posNum[Int]
-      children <- Gen.sequence(Seq.fill(fanOut)(gen(maxDepth - 1)))
-    } yield Node(value, children)
+  private def genNode: Gen[Node] = Gen.sized {
+    case 1 =>
+      Gen const Node(1, Nil)
 
-    gen(maxDepth = 10)
+    case size =>
+      for {
+        width <- Gen choose (1, size - 1)
+        children <- Gen listOfN (width, Gen resize ((size - 1) / width, Gen lzy genNode))
+      } yield Node(1, children)
   }
 
-  private implicit val arbNode = Arbitrary(genNode)
+  implicit val arbNode = Arbitrary(genNode)
+
+  implicit def prettyNode(tree: Node): Pretty = Pretty {
+    _ => "\n" + tree.prettyTree
+  }
 
   test("transformDown") {
-    val tree =
-      Node(1, Seq(
-        Node(2, Seq(
-          Node(4, Nil),
-          Node(5, Nil)
-        )),
-        Node(3, Seq(
-          Node(6, Nil),
-          Node(7, Nil)
-        ))
-      ))
-
-    checkTree(
-      Node(6, Seq(
-        Node(11, Seq(
-          Node(4, Nil),
-          Node(5, Nil)
-        )),
-        Node(16, Seq(
-          Node(6, Nil),
-          Node(7, Nil)
-        ))
-      )),
-
-      tree.transformDown {
-        case child @ Node(i, grandChildren) =>
-          child.copy(value = i + grandChildren.map(_.value).sum)
+    check { tree: Node =>
+      tree transformDown {
+        case node @ Node(_, children) =>
+          node.copy(value = children.map(_.value).sum)
+      } forall {
+        case Node(value, Nil)      => value == 0
+        case Node(value, children) => value == children.size
       }
-    )
+    }
   }
 
   test("transformUp") {
-    val tree =
-      Node(1, Seq(
-        Node(2, Seq(
-          Node(4, Nil),
-          Node(5, Nil)
-        )),
-        Node(3, Seq(
-          Node(6, Nil),
-          Node(7, Nil)
-        ))
-      ))
-
-    checkTree(
-      Node(28, Seq(
-        Node(11, Seq(
-          Node(4, Nil),
-          Node(5, Nil)
-        )),
-        Node(16, Seq(
-          Node(6, Nil),
-          Node(7, Nil)
-        ))
-      )),
-
-      tree.transformUp {
-        case child @ Node(i, grandChildren) =>
-          child.copy(value = i + grandChildren.map(_.value).sum)
+    check { tree: Node =>
+      tree transformUp {
+        case n @ Node(_, children) =>
+          n.copy(value = children.map(_.value).sum)
+      } forall {
+        case Node(value, _) => value == 0
       }
-    )
+    }
   }
 
   test("collect") {
-    check { tree: Node =>
-      val evenNodes = tree.collect { case node @ Node(i, _) if i % 2 == 0 => node }.toSet
-      val oddNodes = tree.collect { case node @ Node(i, _) if i % 2 == 1 => node }.toSet
-      val allNodes = tree.collect { case node => node }.toSet
+    val prop = forAll { tree: Node =>
+      val even = tree collect { case n if n.children.size % 2 == 0 => n }
+      val odd = tree collect { case n if n.children.size % 2 == 1 => n }
+      val nodes = tree collect { case n => n }
 
-      (evenNodes & oddNodes).isEmpty &&
-        (evenNodes ++ oddNodes) == allNodes &&
-        evenNodes.forall { _.value % 2 == 0 } &&
-        oddNodes.forall { _.value % 2 == 1 }
+      all(
+        "all nodes are collected" |:
+          (nodes.size == tree.size),
+
+        "a node can't be both even and odd" |:
+          (even intersect odd).isEmpty,
+
+        "a node must be either even or odd" |:
+          (even.size + odd.size == nodes.size),
+
+        "even nodes are even" |:
+          (even forall (_.children.size % 2 == 0)),
+
+        "odd nodes are odd" |:
+          (odd forall (_.children.size % 2 == 1))
+      )
     }
+
+    check(prop, Test.Parameters.defaultVerbose)
+  }
+
+  test("forall") {
+    check { tree: Node =>
+      all(
+        "the generator we are using only generates nodes with value 1" |:
+          tree.forall(_.value == 1),
+
+        "for trees that has more than 1 node, there must be non-leaf nodes" |:
+          (tree.size > 1) ==> !tree.forall(_.isLeaf)
+      )
+    }
+  }
+
+  test("exists") {
+    check { tree: Node =>
+      all(
+        "a tree must have leaf node(s)" |:
+          tree.exists(_.isLeaf),
+
+        "the generator we are using only generates nodes with value 1" |:
+          !tree.exists(_.value == 2)
+      )
+    }
+  }
+
+  test("size") {
+    check { tree: Node =>
+      tree.size == (tree collect { case n => n.value }).sum
+    }
+  }
+
+  test("depth") {
+    check { tree: Node =>
+      // Computes the depth by iterating over the tree and removing all the leaf nodes during each
+      // iteration until only the root node is left.
+      val iterations = iterate(tree) {
+        _ transformDown {
+          case n => n.copy(children = n.children filterNot (_.isLeaf))
+        }
+      } takeWhile (_.size > 1)
+
+      tree.depth == iterations.size + 1
+    }
+  }
+}
+
+object TreeNodeSuite {
+  case class Node(value: Int, children: Seq[Node]) extends TreeNode[Node] {
+    override def nodeCaption: String = s"Node($value)"
+
+    def isLeaf: Boolean = children.isEmpty
   }
 }
