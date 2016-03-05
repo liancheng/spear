f03b3feae65b843010aece464b4e5e11ed146aeb
Cheng Lian <lian@databricks.com>
2016-03-05 14:44:40 +0800

    Logging support for REPL

--- repl/src/main/resources/predef.scala
+++ repl/src/main/resources/predef.scala
@@ -1,9 +1,15 @@
+import org.apache.log4j.{Level, Logger}
+
 import scraper.config.Settings
 import scraper.local.LocalContext
 import scraper.utils.loadConfig
 
-val context = new LocalContext(new Settings(loadConfig("scraper")))
-
 import scraper.expressions.dsl._
 import scraper.expressions.functions._
 import scraper.types._
+
+val context = new LocalContext(new Settings(loadConfig("scraper")))
+
+def setLogLevel(level: String) {
+  Logger.getRootLogger.setLevel(Level.toLevel(level))
+}
