4d3c96ce1c9829a2394855069a5eeb8133640e24
Cheng Lian <lian@databricks.com>
2016-03-08 15:13:56 +0800

    Reorganizes local physical operators

---
+++ execution/local/src/main/scala/scraper/local/plans/physical/aggregate.scala
@@ -0,0 +1,89 @@
+package scraper.local.plans.physical
+
+import scala.collection.mutable
+
+import scraper.{MutableRow, _}
+import scraper.expressions.BoundRef.bind
+import scraper.expressions.{AggregateFunction, GroupingAlias, NamedExpression, _}
+import scraper.plans.physical.{PhysicalPlan, UnaryPhysicalPlan}
+
+case class HashAggregate(
+  child: PhysicalPlan,
+  groupingList: Seq[GroupingAlias],
+  projectList: Seq[NamedExpression]
+) extends UnaryPhysicalPlan {
+  type AccumulatorMap = Map[AggregateFunction, MutableRow]
+
+  private val hashMap: mutable.HashMap[Row, (MutableRow, AccumulatorMap)] =
+    mutable.HashMap.empty[Row, (MutableRow, AccumulatorMap)]
+
+  private val groupingOutput: Seq[Attribute] = groupingList map (_.toAttribute)
+
+  private val boundGroupingList: Seq[GroupingAlias] = groupingList map (bind(_, child.output))
+
+  private val boundAggregateList: Seq[NamedExpression] =
+    projectList map (bind(_, groupingOutput ++ child.output))
+
+  override def output: Seq[Attribute] = (groupingList ++ projectList) map (_.toAttribute)
+
+  private def allocateAccumulators(
+    aggs: Seq[AggregateFunction]
+  ): (MutableRow, Map[AggregateFunction, MutableRow]) = {
+    val schemaLengths = aggs.map(_.accumulatorSchema.length)
+    val accumulator = new BasicMutableRow(schemaLengths.sum)
+    val begins = schemaLengths.inits.toSeq.reverse.init.map(_.sum)
+    val perAggAccumulators = begins.zip(schemaLengths).map {
+      case (begin, length) => new MutableRowSlice(accumulator, begin, length)
+    }
+    (accumulator, aggs.zip(perAggAccumulators).toMap)
+  }
+
+  override def iterator: Iterator[Row] = {
+    hashMap.clear()
+
+    val boundAggFunctions = for {
+      named <- boundAggregateList
+      agg <- named collect { case f: AggregateFunction => f }
+    } yield agg
+
+    val aggOutput = boundAggFunctions.zipWithIndex.map {
+      case (f, i) =>
+        UnresolvedAttribute(s"agg$i") of f.dataType withNullability f.nullable
+    }
+
+    val rewriteAgg = boundAggFunctions.zip(aggOutput).toMap
+
+    val rewrittenAggList = boundAggregateList.map(_.transformDown {
+      case f: AggregateFunction => rewriteAgg(f)
+    })
+
+    child.iterator foreach { row =>
+      val key = Row.fromSeq(boundGroupingList map (_ evaluate row))
+
+      val accumulators = hashMap.getOrElseUpdate(key, {
+        val (buffer, accumulators) = allocateAccumulators(boundAggFunctions)
+        accumulators.foreach { case (agg, acc) => agg.zero(acc) }
+        (buffer, accumulators)
+      })
+
+      accumulators._2.foreach {
+        case (agg, acc) =>
+          agg.accumulate(acc, row)
+      }
+    }
+
+    val resultRow = new BasicMutableRow(projectList.length)
+    val boundRewrittenAggList = rewrittenAggList.map(bind(_, groupingOutput ++ aggOutput))
+
+    hashMap.iterator.map {
+      case (keyRow, (buffer, _)) =>
+        val joinedRow = new JoinedRow(keyRow, buffer)
+        boundRewrittenAggList.zipWithIndex.foreach {
+          case (a, i) =>
+            resultRow(i) = a.evaluate(joinedRow)
+        }
+
+        new JoinedRow(keyRow, resultRow)
+    }
+  }
+}
--- execution/local/src/main/scala/scraper/local/plans/physical/basicOperators.scala
+++ execution/local/src/main/scala/scraper/local/plans/physical/basicOperators.scala
@@ -1,12 +1,10 @@
 package scraper.local.plans.physical
 
-import scala.collection.mutable
-
+import scraper._
 import scraper.expressions.BoundRef.bind
 import scraper.expressions.Literal.True
 import scraper.expressions._
 import scraper.plans.physical.{BinaryPhysicalPlan, LeafPhysicalPlan, PhysicalPlan, UnaryPhysicalPlan}
-import scraper._
 
 case class LocalRelation(data: Iterable[Row], override val output: Seq[Attribute])
   extends LeafPhysicalPlan {
@@ -98,85 +96,4 @@ case class Sort(child: PhysicalPlan, order: Seq[SortOrder]) extends UnaryPhysica
   private lazy val rowOrdering = new RowOrdering(order, child.output)
 
   override def iterator: Iterator[Row] = child.iterator.toArray.sorted(rowOrdering).toIterator
-}
-
-case class HashAggregate(
-  child: PhysicalPlan,
-  groupingList: Seq[GroupingAlias],
-  aggregateList: Seq[NamedExpression]
-) extends UnaryPhysicalPlan {
-  type AccumulatorMap = Map[AggregateFunction, MutableRow]
-
-  private val hashMap: mutable.HashMap[Row, (MutableRow, AccumulatorMap)] =
-    mutable.HashMap.empty[Row, (MutableRow, AccumulatorMap)]
-
-  private val groupingOutput: Seq[Attribute] = groupingList map (_.toAttribute)
-
-  private val boundGroupingList: Seq[GroupingAlias] = groupingList map (bind(_, child.output))
-
-  private val boundAggregateList: Seq[NamedExpression] =
-    aggregateList map (bind(_, groupingOutput ++ child.output))
-
-  override def output: Seq[Attribute] = (groupingList ++ aggregateList) map (_.toAttribute)
-
-  private def allocateAccumulators(
-    aggs: Seq[AggregateFunction]
-  ): (MutableRow, Map[AggregateFunction, MutableRow]) = {
-    val schemaLengths = aggs.map(_.accumulatorSchema.length)
-    val accumulator = new BasicMutableRow(schemaLengths.sum)
-    val begins = schemaLengths.inits.toSeq.reverse.init.map(_.sum)
-    val perAggAccumulators = begins.zip(schemaLengths).map {
-      case (begin, length) => new MutableRowSlice(accumulator, begin, length)
-    }
-    (accumulator, aggs.zip(perAggAccumulators).toMap)
-  }
-
-  override def iterator: Iterator[Row] = {
-    hashMap.clear()
-
-    val boundAggFunctions = for {
-      named <- boundAggregateList
-      agg <- named collect { case f: AggregateFunction => f }
-    } yield agg
-
-    val aggOutput = boundAggFunctions.zipWithIndex.map {
-      case (f, i) =>
-        UnresolvedAttribute(s"agg$i") of f.dataType withNullability f.nullable
-    }
-
-    val rewriteAgg = boundAggFunctions.zip(aggOutput).toMap
-
-    val rewrittenAggList = boundAggregateList.map(_.transformDown {
-      case f: AggregateFunction => rewriteAgg(f)
-    })
-
-    child.iterator foreach { row =>
-      val key = Row.fromSeq(boundGroupingList map (_ evaluate row))
-
-      val accumulators = hashMap.getOrElseUpdate(key, {
-        val (buffer, accumulators) = allocateAccumulators(boundAggFunctions)
-        accumulators.foreach { case (agg, acc) => agg.zero(acc) }
-        (buffer, accumulators)
-      })
-
-      accumulators._2.foreach {
-        case (agg, acc) =>
-          agg.accumulate(acc, row)
-      }
-    }
-
-    val resultRow = new BasicMutableRow(aggregateList.length)
-    val boundRewrittenAggList = rewrittenAggList.map(bind(_, groupingOutput ++ aggOutput))
-
-    hashMap.iterator.map {
-      case (keyRow, (buffer, _)) =>
-        val joinedRow = new JoinedRow(keyRow, buffer)
-        boundRewrittenAggList.zipWithIndex.foreach {
-          case (a, i) =>
-            resultRow(i) = a.evaluate(joinedRow)
-        }
-
-        new JoinedRow(keyRow, resultRow)
-    }
-  }
 }
