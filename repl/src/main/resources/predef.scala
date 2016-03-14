ecd9897d8bfaf705966ba60f42a34a66aee960a1
Cheng Lian <lian@databricks.com>
2016-03-15 00:17:15 +0800

    Adds default configuration file or REPL

--- repl/src/main/resources/predef.scala
+++ repl/src/main/resources/predef.scala
@@ -11,7 +11,7 @@ import scraper.types._
 
 import scraper.Context._
 
-implicit val context = new LocalContext(new Settings(loadConfig("scraper")))
+implicit val context = new LocalContext(new Settings(loadConfig("full")))
 
 def setLogLevel(level: String) {
   Logger.getRootLogger.setLevel(Level.toLevel(level))
