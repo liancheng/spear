b559119d2f0a7e1d1284e447184b23b13accb6c8
Cheng Lian <lian@databricks.com>
2016-06-09 10:17:44 -0700

    Adds customized REPL banner string

--- repl/src/main/scala/scraper/repl/Main.scala
+++ repl/src/main/scala/scraper/repl/Main.scala
@@ -5,9 +5,31 @@ import scala.io.Source
 import ammonite.repl.{Main => AmmoniteMain}
 
 object Main {
+  private val scalaVersion = scala.util.Properties.versionNumberString
+
+  private val javaVersion = System.getProperty("java.version")
+
+  private val banner =
+    s"""Welcome to
+      |     ____
+      |    / __/__________ ____  ___ ____
+      |   _\\ \\/ __/ __/ _ `/ _ \\/ -_) __/
+      |  /___/\\__/_/  \\_,_/ .__/\\__/_/
+      |                  /_/
+      |
+      |Scala version: $scalaVersion
+      |Java version: $javaVersion
+      |
+      |The default context object is available as `context'.
+      |""".stripMargin
+
   def main(args: Array[String]) {
-    val contextClassLoader = Thread.currentThread().getContextClassLoader
-    val predefStream = contextClassLoader.getResourceAsStream("predef.scala")
-    AmmoniteMain.run(predef = Source.fromInputStream(predefStream, "UTF-8").mkString)
+    val predef = {
+      val contextClassLoader = Thread.currentThread().getContextClassLoader
+      val stream = contextClassLoader.getResourceAsStream("predef.scala")
+      Source.fromInputStream(stream, "UTF-8").mkString
+    }
+
+    AmmoniteMain(predef = predef, welcomeBanner = Some(banner)).run()
   }
 }
