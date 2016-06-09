5809bb67975b6e36411efa4435f2c189b4fcd1c7
Cheng Lian <lian@databricks.com>
2016-06-09 10:17:33 -0700

    Upgrades Ammonite to 0.6.0

--- project/Build.scala
+++ project/Build.scala
@@ -102,7 +102,7 @@ object Dependencies {
   )
 
   object Versions {
-    val ammonite = "0.5.7"
+    val ammonite = "0.6.0"
     val config = "1.2.1"
     val jline = "2.12.1"
     val log4j = "1.2.16"
