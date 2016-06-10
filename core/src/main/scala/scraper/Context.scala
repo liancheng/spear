60912c10698d1a759fb4d0a7f67f51f39438bb20
Cheng Lian <lian@databricks.com>
2016-06-10 09:49:53 -0700

    Moves Catalog and FunctionRegistry into separate source files

--- core/src/main/scala/scraper/Context.scala
+++ core/src/main/scala/scraper/Context.scala
@@ -1,88 +1,14 @@
 package scraper
 
-import scala.collection.{mutable, Iterable}
+import scala.collection.Iterable
 import scala.language.existentials
 import scala.reflect.runtime.universe.WeakTypeTag
 
 import scraper.config.Settings
-import scraper.exceptions.{FunctionNotFoundException, TableNotFoundException}
 import scraper.expressions._
 import scraper.plans.logical.{LocalRelation, LogicalPlan, SingleRowRelation}
-import scraper.plans.logical.dsl._
 import scraper.plans.physical.PhysicalPlan
 import scraper.types.{LongType, StructType}
-
-case class FunctionInfo(
-  name: String,
-  functionClass: Class[_ <: Expression],
-  builder: Seq[Expression] => Expression
-)
-
-object FunctionInfo {
-  def apply[F <: Expression](
-    functionClass: Class[F],
-    builder: Expression => Expression
-  ): FunctionInfo = {
-    val name = (functionClass.getSimpleName stripSuffix "$").toUpperCase
-    FunctionInfo(name, functionClass, (args: Seq[Expression]) => builder(args.head))
-  }
-}
-
-trait FunctionRegistry {
-  def registerFunction(fn: FunctionInfo): Unit
-
-  def removeFunction(name: String): Unit
-
-  def lookupFunction(name: String): FunctionInfo
-}
-
-trait Catalog {
-  val functionRegistry: FunctionRegistry
-
-  def registerRelation(tableName: String, analyzedPlan: LogicalPlan): Unit
-
-  def removeRelation(tableName: String): Unit
-
-  def lookupRelation(tableName: String): LogicalPlan
-}
-
-class InMemoryCatalog extends Catalog {
-  override val functionRegistry: FunctionRegistry = new FunctionRegistry {
-    private val functions: mutable.Map[String, FunctionInfo] =
-      mutable.Map.empty[String, FunctionInfo]
-
-    override def lookupFunction(name: String): FunctionInfo =
-      functions.getOrElse(name.toLowerCase, throw new FunctionNotFoundException(name))
-
-    override def registerFunction(fn: FunctionInfo): Unit = functions(fn.name.toLowerCase) = fn
-
-    override def removeFunction(name: String): Unit = functions -= name
-  }
-
-  private val tables: mutable.Map[String, LogicalPlan] = mutable.Map.empty
-
-  functionRegistry.registerFunction(FunctionInfo(classOf[Rand], Rand))
-  functionRegistry.registerFunction(FunctionInfo(classOf[Count], Count))
-  functionRegistry.registerFunction(FunctionInfo(classOf[Sum], Sum))
-  functionRegistry.registerFunction(FunctionInfo(classOf[Max], Max))
-  functionRegistry.registerFunction(FunctionInfo(classOf[Min], Min))
-  functionRegistry.registerFunction(FunctionInfo(classOf[BoolAnd], BoolAnd))
-  functionRegistry.registerFunction(FunctionInfo(classOf[BoolOr], BoolOr))
-  functionRegistry.registerFunction(FunctionInfo(classOf[Average], Average))
-  functionRegistry.registerFunction(FunctionInfo(classOf[First], First))
-  functionRegistry.registerFunction(FunctionInfo(classOf[Last], Last))
-
-  override def registerRelation(tableName: String, analyzedPlan: LogicalPlan): Unit =
-    tables(tableName) = analyzedPlan
-
-  override def removeRelation(tableName: String): Unit = tables -= tableName
-
-  override def lookupRelation(tableName: String): LogicalPlan =
-    tables
-      .get(tableName)
-      .map(_ subquery tableName)
-      .getOrElse(throw new TableNotFoundException(tableName))
-}
 
 trait Context {
   type QueryExecution <: plans.QueryExecution
---
+++ core/src/main/scala/scraper/FunctionRegistry.scala
@@ -0,0 +1,31 @@
+package scraper
+
+import scala.language.existentials
+
+import scraper.expressions._
+
+case class FunctionInfo(
+  name: String,
+  functionClass: Class[_ <: Expression],
+  builder: Seq[Expression] => Expression
+)
+
+object FunctionInfo {
+  type FunctionBuilder = Seq[Expression] => Expression
+
+  def apply[F <: Expression](
+    functionClass: Class[F],
+    builder: Expression => Expression
+  ): FunctionInfo = {
+    val name = (functionClass.getSimpleName stripSuffix "$").toUpperCase
+    FunctionInfo(name, functionClass, (args: Seq[Expression]) => builder(args.head))
+  }
+}
+
+trait FunctionRegistry {
+  def registerFunction(fn: FunctionInfo): Unit
+
+  def removeFunction(name: String): Unit
+
+  def lookupFunction(name: String): FunctionInfo
+}
