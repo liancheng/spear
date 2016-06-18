7be3e053340443525fea88d849bcb79735aebdcf
Cheng Lian <lian@databricks.com>
2016-06-17 22:23:01 -0700

    More complex example code

--- examples/src/main/scala/BasicExample.scala
+++ examples/src/main/scala/BasicExample.scala
@@ -1,14 +1,33 @@
+import scraper.Context
 import scraper.config.Settings
 import scraper.expressions.dsl._
+import scraper.expressions.functions._
 import scraper.local.LocalContext
 
 object BasicExample {
   def main(args: Array[String]) {
     val context = new LocalContext(Settings.load())
-    val df0 = context range 10
-    val df1 = df0 select ('id as 'a, 'id as 'b) where 'a > 3
 
-    df1.explain()
-    df1.show()
+    simpleQuery(context)
+    aggregateWithHavingAndOrderBy(context)
+  }
+
+  def simpleQuery(context: Context): Unit = {
+    val df = context range 10 select ('id as 'a, 'id as 'b) where 'a > 3
+    df.explain()
+    df.show()
+  }
+
+  def aggregateWithHavingAndOrderBy(context: Context): Unit = {
+    val df = context
+      .range(10)
+      .select('id as 'a, 'id as 'b, 'id as 'c)
+      .groupBy('a)
+      .agg(count('b))
+      .having('a > 3 && max('c) < 4)
+      .orderBy(min('b))
+
+    df.explain()
+    df.show()
   }
 }
