fb20b5e5d51ae4eac39a2c71b45dd94657645345
Cheng Lian <lian@databricks.com>
2016-06-24 18:46:59 +0800

    Refactoring and more tests

--- core/src/main/scala/scraper/package.scala
+++ core/src/main/scala/scraper/package.scala
@@ -1,17 +1,15 @@
 import scala.language.implicitConversions
 
+import scraper.Name.{caseInsensitive, caseSensitive}
+
 package object scraper {
-  implicit def `String->CaseSensitiveName`(string: String): Name =
-    Name.caseSensitive(string)
+  implicit def `String->CaseSensitiveName`(string: String): Name = caseSensitive(string)
 
-  implicit def `Symbol->CaseInsensitiveName`(symbol: Symbol): Name =
-    Name.caseInsensitive(symbol.name)
+  implicit def `Symbol->CaseInsensitiveName`(symbol: Symbol): Name = caseInsensitive(symbol.name)
 
-  implicit class StringToCaseSensitiveName(sc: StringContext) {
-    def cs(args: Any*): Name = Name.caseSensitive(sc.s(args: _*))
-  }
+  implicit class NameHelper(sc: StringContext) {
+    def cs(args: Any*): Name = caseSensitive(sc.s(args: _*))
 
-  implicit class StringToCaseInsensitiveName(sc: StringContext) {
-    def ci(args: Any*): Name = Name.caseInsensitive(sc.s(args: _*))
+    def ci(args: Any*): Name = caseInsensitive(sc.s(args: _*))
   }
 }
--- core/src/test/scala/scraper/NameSuite.scala
+++ core/src/test/scala/scraper/NameSuite.scala
@@ -11,48 +11,48 @@ import scraper.Name.{caseInsensitive, caseSensitive}
 class NameSuite extends LoggingFunSuite with Checkers {
   test("equality") {
     check(all(
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         (name != name.toLowerCase) ==> {
           "inequality: case-insensitive v.s. case-sensitive" |:
             caseInsensitive(name.toLowerCase) != caseSensitive(name)
         }
       },
 
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         (name != name.toLowerCase) ==> {
           "inequality: case-sensitive v.s. case-insensitive" |:
             caseSensitive(name.toLowerCase) != caseInsensitive(name)
         }
       },
 
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         (name != name.toLowerCase) ==> {
           "inequality: case-sensitive v.s. case-sensitive" |:
             caseSensitive(name.toLowerCase) != caseSensitive(name)
         }
       },
 
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         "equality: case-sensitive v.s. case-sensitive" |:
           caseSensitive(name) == caseSensitive(name)
       },
 
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         "equality: case-sensitive v.s. case-insensitive" |:
           caseSensitive(name) == caseInsensitive(name)
       },
 
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         "equality: case-insensitive v.s. case-sensitive" |:
           caseInsensitive(name) == caseSensitive(name)
       },
 
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         "equality: case-insensitive v.s. case-insensitive (in the same case)" |:
           caseInsensitive(name) == caseInsensitive(name)
       },
 
-      forAll(alphaStr) { name: String =>
+      forAll(alphaStr) { name =>
         "equality: case-insensitive v.s. case-insensitive (in different cases)" |:
           caseInsensitive(name) == caseInsensitive(name.toLowerCase)
       }
@@ -67,10 +67,33 @@ class NameSuite extends LoggingFunSuite with Checkers {
 
     check {
       forAll(genName, genName) {
-        case (lhs: Name, rhs: Name) if lhs == rhs => lhs.## == rhs.##
         case (lhs: Name, rhs: Name) if lhs.## != rhs.## => lhs != rhs
+        // TODO Probability of reaching this branch is too small, can't be well tested
+        case (lhs: Name, rhs: Name) if lhs == rhs => lhs.## == rhs.##
         case _ => true
       }
     }
+  }
+
+  test("interpolation") {
+    val caseSensitive = cs"Hello"
+    val caseInsensitive = ci"Hello"
+
+    assert(caseSensitive.isCaseSensitive)
+    assert(caseSensitive.casePreserving == "Hello")
+
+    assert(!caseInsensitive.isCaseSensitive)
+    assert(caseInsensitive.casePreserving == "Hello")
+  }
+
+  test("implicit conversion") {
+    val caseSensitive: Name = "Hello"
+    val caseInsensitive: Name = 'Hello
+
+    assert(caseSensitive.isCaseSensitive)
+    assert(caseSensitive.casePreserving == "Hello")
+
+    assert(!caseInsensitive.isCaseSensitive)
+    assert(caseInsensitive.casePreserving == "Hello")
   }
 }
