add5e04a6191695139f2902be80cfafc51a56377
Cheng Lian <lian@databricks.com>
2016-07-12 15:41:49 +0800

    Better example code

--- examples/src/main/scala/BasicExample.scala
+++ examples/src/main/scala/BasicExample.scala
@@ -1,33 +1,28 @@
-import scraper.Context
 import scraper.config.Settings
 import scraper.expressions.dsl._
 import scraper.expressions.functions._
 import scraper.local.LocalContext
 
 object BasicExample {
+  case class Person(name: String, gender: String, age: Int)
+
   def main(args: Array[String]) {
     val context = new LocalContext(Settings.load())
+    val people = context lift (
+      Person("Alice", "F", 9),
+      Person("Bob", "M", 15),
+      Person("Charlie", "M", 18),
+      Person("David", "M", 13),
+      Person("Eve", "F", 20),
+      Person("Frank", "M", 19)
+    )
 
-    simpleQuery(context)
-    aggregateWithHavingAndOrderBy(context)
-  }
+    val adults = people where 'age >= 18 select ('name, 'gender)
+    adults.explain()
+    adults.show()
 
-  def simpleQuery(context: Context): Unit = {
-    val df = context range 10 select ('id as 'a, 'id as 'b) where 'a > 3
-    df.explain()
-    df.show()
-  }
-
-  def aggregateWithHavingAndOrderBy(context: Context): Unit = {
-    val df = context
-      .range(10)
-      .select('id as 'a, 'id as 'b, 'id as 'c)
-      .groupBy('a)
-      .agg(count('b))
-      .having('a > 3 && max('c) < 4)
-      .orderBy(min('b))
-
-    df.explain()
-    df.show()
+    val countGender = people groupBy 'gender agg ('gender, count() as 'count)
+    countGender.explain()
+    countGender.show()
   }
 }
