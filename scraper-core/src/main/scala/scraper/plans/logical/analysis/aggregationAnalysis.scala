13b4afba88ca9b25f6ddea5cfa1d6be9920d7b1e
Cheng Lian <lian.cs.zju@gmail.com>
2016-09-22 19:43:43 +0800

    Remove all the lengthy comments before refactoring
    Something's probably wrong if you need so lengthy comments. Trying to figure that out and refactor it.

--- scraper-core/src/main/scala/scraper/plans/logical/analysis/aggregationAnalysis.scala
+++ scraper-core/src/main/scala/scraper/plans/logical/analysis/aggregationAnalysis.scala
@@ -136,30 +136,22 @@ class ResolveAggregates(val catalog: Catalog) extends AnalysisRule {
     case plan: UnresolvedAggregate if hasDistinctAggregateFunction(plan.projectList) => ()
   }
 
-  // This partial function is the one who does all the real heavy work. It's one of the most complex
-  // and subtlest piece of code throughout the whole project... Aggregation, what a beast...
   private val resolveUnresolvedAggregate: PartialFunction[LogicalPlan, LogicalPlan] = {
     case agg @ UnresolvedAggregate(Resolved(child), keys, projectList, conditions, order) =>
-      // Aliases all grouping keys and builds a grouping key rewriter map.
       val keyAliases = keys map (GroupingAlias(_))
       val keyRewriter = buildRewriter(keyAliases)
       val keyRestorer = buildRestorer(keyAliases)
 
-      // A function that rewrites all grouping keys in a given expression to their corresponding
-      // `GroupingAttribute`s except for grouping keys appearing as (part of) arguments of non-
-      // window aggregate functions.
       val rewriteKeys = (_: Expression) transformUp keyRewriter transformUp {
         case e: AggregateFunction => e transformUp keyRestorer
         case e: WindowFunction    => e.copy(function = e.function transformUp keyRewriter)
       }
 
-      // Collects and aliases all aggregate functions and builds an aggregate function rewriter map.
       val aggs = collectAggregateFunctions(projectList ++ conditions ++ order map rewriteKeys)
       val aggAliases = aggs map (AggregationAlias(_))
       val aggRewriter = buildRewriter(aggAliases)
       val aggRestorer = buildRestorer(aggAliases)
 
-      // Checks for invalid nested aggregate functions like `MAX(COUNT(*))`.
       aggs foreach rejectNestedAggregateFunction
 
       val rewriteAggs = (_: Expression) transformUp aggRewriter transformUp {
@@ -167,10 +159,6 @@ class ResolveAggregates(val catalog: Catalog) extends AnalysisRule {
           e.copy(function = e.function transformUp aggRestorer)
       }
 
-      // Collects and aliases all window functions. Note that grouping keys referenced by the window
-      // functions and their window specs must be rewritten to `GroupingAttribute`s first before
-      // aliasing. This is because window functions are evaluated after aggregation, thus grouping
-      // keys can only be referenced by window functions in the form of `GroupingAttribute`s.
       val wins = collectWindowFunctions(projectList ++ order map (rewriteKeys andThen rewriteAggs))
       val winAliases = wins map (WindowAlias(_))
       val winRewriter = buildRewriter(winAliases)
@@ -178,124 +166,33 @@ class ResolveAggregates(val catalog: Catalog) extends AnalysisRule {
 
       val rewriteWins = (_: Expression) transformUp winRewriter
 
-      // A function that rewrites aggregate functions, grouping keys, and window functions to
-      // corresponding `InternalAttribute`s. The order of transformations is significant.
       val rewrite = rewriteKeys andThen rewriteAggs andThen rewriteWins
 
-      // A function used to restore `InternalAttribute`s to the original expressions for error
-      // reporting purposes. Same as above, the order of transformations is significant.
       val restore = (_: Expression)
         .transformUp(winRestorer)
         .transformUp(aggRestorer)
         .transformUp(keyRestorer)
 
-      // While being used in aggregations, the only input expressions a window function (and its
-      // window specs) can reference are the grouping keys. E.g., these queries are valid:
-      //
-      //   SELECT max(a) OVER (PARTITION BY a % 10) FROM t GROUP BY a
-      //              ~                     ~                       ~
-      //   SELECT sum(a % 10 + 1) OVER (ORDER BY a % 10 DESC) FROM t GROUP BY a % 10
-      //              ~~~~~~                     ~~~~~~                       ~~~~~~
-      //
-      // while these are not:
-      //
-      //   -- `b` is not a grouping key
-      //   SELECT max(b) OVER (...) FROM t GROUP BY a
-      //
-      //   -- `a` is not a grouping key
-      //   SELECT sum(a) OVER (...) FROM t GROUP BY a % 10
-      //
-      // Therefore, after rewriting all grouping keys to their corresponding `GroupingAttribute`s,
-      // no other input attributes should be found in the rewritten window functions anymore.
       rejectDanglingAttributes("window function", wins map (_.function), keys, Nil, restore)
 
-      // Rewrites grouping keys, aggregate functions, and window functions appearing in having
-      // condition, sort ordering expressions, and projected named expressions to corresponding
-      // `GroupingAttribute`s, `AggregationAttribute`s, and `WindowAttribute`s.
       val rewrittenConditions = conditions map rewrite
       val rewrittenOrder = order map (ord => ord.copy(child = rewrite(ord.child)))
       val rewrittenProjectList = projectList map rewrite zip projectList map {
-        // Top level `InternalAttribute`s must be aliased back to names and expression IDs of the
-        // original named expressions so that the transformed plan still has the same output
-        // attributes as the original plan.
         case (g: InternalAttribute, e) => g as e.name withID e.expressionID
         case (e: NamedExpression, _)   => e
       }
 
-      // Expressions appearing in an aggregation must conform to the following constraints:
-      //
-      //  1. An input attribute appearing in a projected named expression must appear as either
-      //
-      //     * part of a grouping key, or
-      //     * (part of) an argument of a non-window aggregate function.
-      //
-      //     E.g., these queries are valid:
-      //
-      //       -- `a` appears as part of grouping key `a + 1`.
-      //       SELECT a + 1 FROM t GROUP BY a + 1
-      //       SELECT a + 1 + 1 FROM t GROUP BY a + 1
-      //       SELECT a + 1 + (a + 1) FROM t GROUP BY a + 1
-      //
-      //       -- `b` appears as the argument of non-window aggregate function `max`.
-      //       SELECT max(b) FROM t GROUP BY a + 1
-      //
-      //       -- `b` appears as part of the argument of non-window aggregate function `avg`.
-      //       SELECT avg(b % 10) FROM t GROUP BY a + 1
-      //
-      //     while these are not:
-      //
-      //       -- `a + 2` is not a grouping key.
-      //       SELECT a + 2 FROM t GROUP BY a + 1
-      //
-      //       -- The 2nd `a` is not part of the grouping key `a + 1`. Note that the fully
-      //       -- parenthesized form of `a + 1 + a + 1` is `(((a + 1) + a) + 1)`.
-      //       SELECT a + 1 + a + 1 FROM t GROUP BY a + 1
-      //
-      //       -- `b` appears as the argument of window aggregate function `max(b) OVER (...)`.
-      //       SELECT max(b) OVER (...) FROM t GROUP BY a + 1
-      //
-      //  2. An input attribute appearing in HAVING and ORDER BY clauses must appear as
-      //
-      //     * part of a grouping key, or
-      //     * (part of) an argument of a non-window aggregate function, or
-      //     * an output attribute of the project list.
-      //
-      //     E.g., these queries are valid:
-      //
-      //       SELECT max(b) AS max FROM t GROUP BY a + 1 ORDER BY a + 1 DESC
-      //                                            ~~~~~          ~~~~~
-      //       SELECT max(b) AS max FROM t GROUP BY a + 1 HAVING sum(a) < 10
-      //                                                         ~~~~~~
-      //       SELECT max(b) AS max FROM t GROUP BY a + 1 HAVING max > 0
-      //              ~~~~~~~~~~~~~                              ~~~
-      //
-      //     while these are not:
-      //
-      //       -- `a` is not a projected output attribute and `a + 2` is not a grouping key.
-      //       SELECT max(b) AS max FROM t GROUP BY a + 1 HAVING a + 2 < 10
-      //
-      //       -- `b` is neither a projected output attribute nor a grouping key.
-      //       SELECT max(b) AS max FROM t GROUP BY a + 1 ORDER BY b DESC
-      //
-      // At this stage, we've already rewritten all grouping keys and aggregate functions to
-      // corresponding `GroupingAttribute`s and `AggregationAttribute`s. Therefore, no other input
-      // attributes should be found in these rewritten expressions anymore.
       val output = rewrittenProjectList map (_.toAttribute)
       rejectDanglingAttributes("SELECT field", rewrittenProjectList, keys, Nil, restore)
       rejectDanglingAttributes("HAVING condition", rewrittenConditions, keys, output, restore)
       rejectDanglingAttributes("ORDER BY expression", rewrittenOrder, keys, output, restore)
 
       child
-        // The main aggregation.
         .resolvedGroupBy(keyAliases)
         .agg(aggAliases)
-        // `HAVING` clause.
         .filterOption(rewrittenConditions)
-        // Stacks one `Window` operator for each window spec.
         .windowsOption(winAliases)
-        // `ORDER BY` clause.
         .orderByOption(rewrittenOrder)
-        // Evaluates all non-window and non-aggregate expressions and cleans up output attributes.
         .select(rewrittenProjectList)
   }
 
