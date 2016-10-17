14a51743b3694844a5fbd38af7ae39c7f17873c3
Cheng Lian <lian.cs.zju@gmail.com>
2016-10-16 23:17:55 -0700

    Fix REPL predef

--- scraper-repl/src/main/resources/predef.scala
+++ scraper-repl/src/main/resources/predef.scala
@@ -1,15 +1,15 @@
 import org.apache.log4j.{Level, Logger}
 
+import scraper.Context
 import scraper.config.Settings
 import scraper.expressions._
 import scraper.expressions.dsl._
 import scraper.expressions.functions._
 import scraper.expressions.windows._
-import scraper.local.LocalContext
 import scraper.repl.Main.%
 import scraper.types._
 
-implicit val context = new LocalContext(Settings.load("scraper.conf", "scraper-reference.conf"))
+implicit val context = new Context(Settings.load("scraper.conf", "scraper-reference.conf"))
 
 import context._
 
