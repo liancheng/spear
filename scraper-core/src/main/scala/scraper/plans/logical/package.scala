025c0c8efd9541717b3257e873154ca0b3ad8a85
Cheng Lian <lian.cs.zju@gmail.com>
2016-11-02 10:30:19 -0700

    Remove unused method

--- scraper-core/src/main/scala/scraper/plans/logical/package.scala
+++ scraper-core/src/main/scala/scraper/plans/logical/package.scala
@@ -10,11 +10,6 @@ package object logical {
 
   def values(first: Expression, rest: Expression*): Project = values(first +: rest)
 
-  def let(cteRelation: (Symbol, LogicalPlan))(body: LogicalPlan): With = {
-    val (name, value) = cteRelation
-    With(body, name, value)
-  }
-
   def let(name: Name, plan: LogicalPlan)(body: => LogicalPlan): With = {
     With(body, name, plan)
   }
