a4f155c57fc75e9a8838d97cd8d4c801aeecac3b
Cheng Lian <lian.cs.zju@gmail.com>
2017-01-28 14:10:16 -0800

    Use standard || operator in SQL representation of Concat

--- scraper-core/src/main/scala/scraper/expressions/stringExpressions.scala
+++ scraper-core/src/main/scala/scraper/expressions/stringExpressions.scala
@@ -2,9 +2,12 @@ package scraper.expressions
 
 import java.util.regex.Pattern
 
+import scala.util.Try
+
 import scraper.Row
 import scraper.expressions.typecheck.{Foldable, TypeConstraint}
 import scraper.types.{BooleanType, DataType, StringType}
+import scraper.utils.trySequence
 
 case class Concat(children: Seq[Expression]) extends Expression {
   override def dataType: DataType = StringType
@@ -13,6 +16,9 @@ case class Concat(children: Seq[Expression]) extends Expression {
 
   override def evaluate(input: Row): Any =
     (children map (_ evaluate input) map (_.asInstanceOf[String]) filter (_ != null)).mkString
+
+  override def sql: Try[String] =
+    trySequence(children.map { _.sql }) map { _ mkString ("(", " || ", ")") }
 }
 
 case class RLike(left: Expression, right: Expression) extends BinaryOperator {
