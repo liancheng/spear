522bfb2ad205a6061b9523a54e96c048a178245d
Cheng Lian <lian.cs.zju@gmail.com>
2017-06-25 12:56:22 -0700

    Move LoggingFunSuite to spear-utils

--- spear-utils/src/test/scala/spear/LoggingFunSuite.scala
+++ spear-utils/src/test/scala/spear/LoggingFunSuite.scala
@@ -4,7 +4,7 @@ import org.scalatest.{FunSuite, Outcome}
 
 import spear.utils.Logging
 
-trait LoggingFunSuite extends FunSuite with Logging {
+class LoggingFunSuite extends FunSuite with Logging {
   override protected def withFixture(test: NoArgTest): Outcome = {
     try {
       logInfo(s"Log output for test '${test.text}' {{{")
